faf4111ca006a663d38de9427308a7853649dd24
Smit-Solanki-EnactOn <sssolanki@enacton.email>
2026-01-30 12:01:57 +0530

    fix: revert back the changes in Users component

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,13 +1,8 @@
-import { useSetAtom } from 'jotai'
-import { themeAtom } from '../app/page'
 import React from 'react'
 
 const Users = () => {
-  const setTheme = useSetAtom(themeAtom);
   return (
-    <div>
-      {setTheme}
-    </div>
+    <div>Users</div>
   )
 }
 
